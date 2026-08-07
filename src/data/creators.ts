6a9a46e9397abd545b31d4a06a3a9e1d93c40de8
Mophygame2 <mophygame2@gamil.com>
2026-08-08 01:46:36 +0800

    fix gif limit problem

--- src/data/creators.ts
+++ src/data/creators.ts
@@ -151,9 +151,9 @@ friendshipCreators[0]={
   discordUrl:"https://discord.gg/5Q94M7Gtua",
   profileUrl:"https://caveduck.io/user/H_mori",
   recommendedWorks:[
-    {title:"真實瑟瑟聲音工作室",hashtags:["NSFW","劇情","高肉"],url:"",image:"/assets/work/黃森_真實瑟瑟聲音工作室.jpg"},
+    {title:"真實瑟瑟聲音工作室",hashtags:["NSFW","劇情","高肉"],url:"",image:"/assets/work/黃森_真實瑟瑟聲音工作室_optimized.gif"},
     {title:"白言沐",hashtags:["劇情","虐戀"],url:"",image:"/assets/work/黃森_白言沐.webp"},
-    {title:"死囚軍團阿維斯塔",hashtags:["恐怖","劇情","搞笑"],url:"",image:"/assets/work/黃森_死囚軍團阿維斯塔.jpg"},
+    {title:"死囚軍團阿維斯塔",hashtags:["恐怖","劇情","搞笑"],url:"",image:"/assets/work/黃森_死囚軍團阿維斯塔.gif"},
   ],
 }
 friendshipCreators[1]={
